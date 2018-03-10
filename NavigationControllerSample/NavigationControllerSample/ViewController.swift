ecc4b9fdf01e5679c7edb8d9db38d708ebe8fc0a
HIRO-NET <hiros.net1@gmail.com>
2018-03-10 15:30:23 +0900

    Navigation Controllerによる画面遷移のサンプル

---
+++ NavigationControllerSample/NavigationControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationControllerSample
+//
+//  Created by 高橋広樹 on 2018/03/07.
+//  Copyright © 2018年 HIRO's.NET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
